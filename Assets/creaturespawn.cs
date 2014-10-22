3be022bb44dae95690624ff567b020d0a5455403
vamsi krishna <medishetty123@gmail.com>
2014-10-22 01:32:30 -0700

    added creatures

---
+++ Assets/creaturespawn.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class creaturespawn : MonoBehaviour {
+	public int maxc=1;
+	public int val=0;
+	public Transform prefab;
+	public float x;
+	public float y;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		x = Random.Range (-100f, 100f);
+		y = Random.Range (-100f, 100f);
+		if (((x * x) + (y * y) > 68f * 68f) ) {
+					
+			if(val<maxc){
+				val+=1;
+			Instantiate (prefab, new Vector3 (x, y, 0), Quaternion.identity);
+			}
+			
+		
+		}
+	
+		}
+	public void subs(){
+		val -= 1;
+
+		}
+}
---
+++ Assets/lookat.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lookat: MonoBehaviour {
+	public Transform target;
+	public Vector3 dir;
+	public int vel=0;
+	public GameObject cube;
+	public creaturespawn csn;
+	void Start(){
+		cube = GameObject.FindGameObjectWithTag ("spawn");
+		csn = cube.GetComponent<creaturespawn>();
+		dir = target.position - transform.position;
+		dir.Normalize ();
+		}
+	void Update() {
+
+
+
+		transform.position += dir * vel * Time.deltaTime;
+		if ((Mathf.Abs(transform.position.x-target.position.x)<0.5)&&(Mathf.Abs(transform.position.y-target.position.y)<0.5)) {
+			csn.subs();
+			Destroy(gameObject);	}
+		var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+	}
+	void OnMouseDown()
+	{
+		csn.subs ();
+		Destroy(gameObject);	
+		
+	}
+}
